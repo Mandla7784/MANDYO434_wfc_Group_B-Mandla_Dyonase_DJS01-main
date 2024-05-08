0b90aed4d4548c28dff91cd29fdfa2b024bcd6c5
Mandla Dyonase <Marsatti7784@gmail.com>
2024-05-08 09:23:49 +0000

    declared variable with descriptive names

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const velocityInKilometersPerHour = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
+const timeInSeconds = 3600; // seconds (1 hour)
+const distanceInKilometers = 0; // distance (km)
+const remainingFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 const d2 = d + vel * time; //calcultes new distance
 const rf = fbr * time; //calculates remaining fuel
